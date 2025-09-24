8e6c4b98b7ec95e62eb8b5ff4abb109b6d159415
Lif Pro <ysalissou@gmail.com>
2025-09-24 12:19:31 +0000

    supabase/postgres

--- supabase/db/jwt.sql
+++ supabase/db/jwt.sql
@@ -1,5 +1,5 @@
-\set jwt_secret `echo "$JWT_SECRET"`
-\set jwt_exp `echo "$JWT_EXP"`
+\set jwt_secret 'super-secret-jwt-token-with-at-least-32-characters-long'
+\set jwt_exp '3600'
 
 ALTER DATABASE postgres SET "app.settings.jwt_secret" TO :'jwt_secret';
 ALTER DATABASE postgres SET "app.settings.jwt_exp" TO :'jwt_exp';
